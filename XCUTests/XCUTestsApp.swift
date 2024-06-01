6e659b478c69435cb14b7c484dff54f5d0a96369
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-01 20:54:44 +0200

    Initial Commit

---
+++ XCUTests/XCUTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCUTestsApp.swift
+//  XCUTests
+//
+//  Created by Maziar Layeghkar on 01.06.24.
+//
+
+import SwiftUI
+
+@main
+struct XCUTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
